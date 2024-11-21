0d31d1e86ffa8bbfb9daac623a33e732fce68183
Vitor P. <100450996+vitorprando@users.noreply.github.com>
2024-11-21 17:34:46 -0300

    Letra maiúscula nos arquivo q deram erro

---
+++ src/components/validations/DescriptionInput.vue
@@ -0,0 +1,13 @@
+<script>
+export function descriptionInput(description) {
+  const trimmedDescription = description.trim();
+  if (trimmedDescription.length <= 0 || trimmedDescription.length > 1000) {
+    return {
+      valid: false,
+      errorValue: "A descrição deve ter no mínimo 1 caractere e no máximo 1000 caracteres.",
+    };
+  }
+  return { valid: true, errorValue: "" };
+}
+
+</script>
